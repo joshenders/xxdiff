6702fde84a5dbd565142ff6b78fc331f1c33e479
Josh Enders <josh.enders@gmail.com>
2020-05-16 05:30:57 +0000

    applies black code formatting

--- xxdd.py
+++ xxdd.py
@@ -8,13 +8,19 @@ import re
 
 from unidiff import PatchSet
 
+
 def print_script(outfile, patch_bytes):
-    patch_map = ' '.join(['{}:{}'.format(key, value) for key, value in patch_bytes.items()])
+    patch_map = " ".join(
+        ["{}:{}".format(key, value) for key, value in patch_bytes.items()]
+    )
 
     # minified version of unminified.sh
-    payload = bytearray(f"for item in {patch_map}; do "
-                        f"dd bs=1 count=1 conv=notrunc seek=$((16#${{item%:*}})) "
-                        f"if=<(printf %b \"\\x${{item#*:}}\") of='{outfile}'; done", 'utf-8')
+    payload = bytearray(
+        f"for item in {patch_map}; do"
+        f" dd bs=1 count=1 conv=notrunc seek=$((16#${{item%:*}}))"
+        f" if=<(printf %b \"\\x${{item#*:}}\") of='{outfile}'; done",
+        "utf-8",
+    )
 
     compressed = gzip.compress(payload)
     encoded = base64.b64encode(compressed).decode()
@@ -30,7 +36,7 @@ def main():
     patch = PatchSet(diff)
     source_file = patch[0].source_file
 
-    pattern = re.compile(r'^[+-]([0-9a-f]+):\ ([0-9a-f\ ]+)\ +.*$')
+    pattern = re.compile(r"^[+-]([0-9a-f]+):\ ([0-9a-f\ ]+)\ +.*$")
     changes = {}
 
     for hunk in patch[0]:
@@ -38,32 +44,36 @@ def main():
             match = pattern.match(line)
             if match:
                 offset = match.group(1)
-                buffer = match.group(2).replace(' ', '')
+                buffer = match.group(2).replace(" ", "")
                 changes[offset] = {}
-                changes[offset]['source'] = [str(buffer[i:i + 2]) for i in range(0, len(buffer), 2)]
+                changes[offset]["source"] = [
+                    str(buffer[i : i + 2]) for i in range(0, len(buffer), 2)
+                ]
 
         for line in hunk.target:
             match = pattern.match(line)
             if match:
                 offset = match.group(1)
-                buffer = match.group(2).replace(' ', '')
-                changes[offset]['target'] = [str(buffer[i:i + 2]) for i in range(0, len(buffer), 2)]
+                buffer = match.group(2).replace(" ", "")
+                changes[offset]["target"] = [
+                    str(buffer[i : i + 2]) for i in range(0, len(buffer), 2)
+                ]
 
     patch_bytes = {}
 
     for offset in changes:
-        #print(f'offset: {offset}')
+        # print(f'offset: {offset}')
         for i in range(16):
-            source_byte = changes[offset]['source'][i]
-            target_byte = changes[offset]['target'][i]
-            #print(f'i: {i}')
+            source_byte = changes[offset]["source"][i]
+            target_byte = changes[offset]["target"][i]
+            # print(f'i: {i}')
             if source_byte != target_byte:
-                cursor = format(int(offset, base=16) + i, 'x')
+                cursor = format(int(offset, base=16) + i, "x")
                 patch_bytes[cursor] = target_byte
-                #print(f'cursor: {cursor}, source_byte: {source_byte}, target_byte: {target_byte}')
+                # print(f'cursor: {cursor}, source_byte: {source_byte}, target_byte: {target_byte}')
 
     print_script(source_file, patch_bytes)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
